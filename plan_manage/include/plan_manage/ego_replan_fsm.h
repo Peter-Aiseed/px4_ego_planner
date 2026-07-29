cb91c38ce5a406e14adf84eabda1b39d737be5f3
kccccck <kcliu@aiseedtech.com>
2026-07-29 18:12:04 +0800

    add initial log utility

--- plan_manage/include/plan_manage/ego_replan_fsm.h
+++ plan_manage/include/plan_manage/ego_replan_fsm.h
@@ -20,6 +20,8 @@
 #include <plan_manage/planner_manager.h>
 #include <traj_utils/planning_visualization.h>
 
+#include <std_msgs/String.h>
+
 using std::vector;
 
 namespace ego_planner
@@ -84,6 +86,7 @@ namespace ego_planner
     ros::Timer exec_timer_, safety_timer_;
     ros::Subscriber waypoint_sub_, odom_sub_, swarm_trajs_sub_, broadcast_bspline_sub_, trigger_sub_;
     ros::Publisher replan_pub_, new_pub_, bspline_pub_, data_disp_pub_, swarm_trajs_pub_, broadcast_bspline_pub_;
+    ros::Publisher fsm_state_pub_;
 
     /* helper functions */
     bool callReboundReplan(bool flag_use_poly_init, bool flag_randomPolyTraj); // front-end and back-end method
