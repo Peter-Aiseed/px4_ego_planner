9be6210b8f9fb463a0d9b9cf36bdbfaf32875423
Peter Cheng <petercheng@aiseedtech.com>
2026-07-30 12:58:39 +0800

    Fix package references in record script.

--- scripts/record.sh
+++ scripts/record.sh
@@ -3,6 +3,6 @@ STAMP=$(date +%F_%H-%M-%S)
 DIR=/ws_ego/src/px4_ego_planner/rosbag
 mkdir -p "$DIR"
 rosparam dump "$DIR/ego_debug_$STAMP.params.yaml"                       # planner must already be running
-cp "$(rospack find ego_planner)/launch/run_ego_real.launch" \
+cp "$(rospack find px4_ego_planner)/launch/run_ego_real.launch" \
    "$DIR/ego_debug_$STAMP.launch"
-roslaunch ego_planner record.launch bag_prefix:="ego_debug_$STAMP"
+roslaunch px4_ego_planner record.launch bag_prefix:="ego_debug_$STAMP"
